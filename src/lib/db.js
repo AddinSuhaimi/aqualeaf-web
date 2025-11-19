61724a63c1655a41a43318e8d5d091dab7cba399
AddinSuhaimi <addinsuhaimi@gmail.com>
2025-11-19 09:07:46 +0800

    added cloud connection support

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,20 @@
 // lib/db.js
 import mysql from 'mysql2/promise';
+import fs from 'fs';
+import path from 'path';
+
+const caPath = path.join(process.cwd(), 'certs', 'ca.pem');
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(caPath)
+  },
+  connectTimeout: 20000,
 });
 
 export default pool;
